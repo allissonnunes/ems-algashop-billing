d6f8655ff7eb641252d03352812dffa42d6e9727
Allisson Alves Batista Nunes <allissonabn@gmail.com>
2026-01-04 19:11:21 -0300

    Add JPA repositories for `Invoice` and `CreditCard` domain models in Billing microservice

---
+++ src/main/java/com/github/allisson95/algashop/billing/domain/model/creditcard/CreditCardRepository.java
@@ -0,0 +1,9 @@
+package com.github.allisson95.algashop.billing.domain.model.creditcard;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface CreditCardRepository extends JpaRepository<CreditCard, UUID> {
+
+}
