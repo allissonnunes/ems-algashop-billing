ba2c9044945a4cb8610658d73265c73a162ce46d
Allisson Alves Batista Nunes <allissonabn@gmail.com>
2026-02-25 17:00:04 -0300

    Update invoice logic to clear expiration dates and enable Flyway test data
    Enhanced invoice domain model to reset `expiresAt` upon payment or cancellation. Enabled Flyway to import test data (`afterMigrate.sql`) for local development. Updated application configuration and added SQL entries for credit cards, payment settings, and invoices.

--- src/main/java/br/dev/allissonnunes/algashop/billing/domain/model/invoice/Invoice.java
+++ src/main/java/br/dev/allissonnunes/algashop/billing/domain/model/invoice/Invoice.java
@@ -110,6 +110,7 @@ public class Invoice extends AbstractAggregateRoot<Invoice> {
             throw new DomainException("Invoice %s with status %s cannot be marked as paid".formatted(this.id, this.status.toString().toLowerCase(Locale.ROOT)));
         }
         this.setPaidAt(Instant.now());
+        this.setExpiresAt(null);
         this.setStatus(InvoiceStatus.PAID);
         super.registerEvent(new InvoicePaidEvent(this.getId(), this.getCustomerId(), this.getOrderId(), this.getPaidAt()));
     }
@@ -120,6 +121,7 @@ public class Invoice extends AbstractAggregateRoot<Invoice> {
         }
         this.setCancellationReason(cancellationReason);
         this.setCanceledAt(Instant.now());
+        this.setExpiresAt(null);
         this.setStatus(InvoiceStatus.CANCELED);
         super.registerEvent(new InvoiceCanceledEvent(this.getId(), this.getCustomerId(), this.getOrderId(), this.getCanceledAt()));
     }
